66ac9ae75a96dc57f90af2f3298e9675d5c1581b
Aldivio Alves <aldivio89@hotmail.com>
2023-07-13 20:59:39 -0300

    init project

---
+++ app/src/main/java/br/com/aldivio/estudos/instagram/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package br.com.aldivio.estudos.instagram.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.aldivio.estudos.instagram.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
