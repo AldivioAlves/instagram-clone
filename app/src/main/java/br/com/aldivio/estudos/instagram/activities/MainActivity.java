701626ce722b8008239e949097c915f3c662f335
Aldivio Alves <aldivio89@hotmail.com>
2023-07-19 19:03:25 -0300

    set main navigation

--- app/src/main/java/br/com/aldivio/estudos/instagram/activities/MainActivity.java
+++ app/src/main/java/br/com/aldivio/estudos/instagram/activities/MainActivity.java
@@ -11,12 +11,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentTransitionImpl;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.aldivio.estudos.instagram.R;
+import br.com.aldivio.estudos.instagram.fragments.FeedFragment;
+import br.com.aldivio.estudos.instagram.fragments.PostFragment;
+import br.com.aldivio.estudos.instagram.fragments.ProfileFragment;
+import br.com.aldivio.estudos.instagram.fragments.SearchFragment;
 import br.com.aldivio.estudos.instagram.helpers.FirebaseUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,30 +45,34 @@ public class MainActivity extends AppCompatActivity {
         mainToolbar.setTitle("Instagram");
         setSupportActionBar(mainToolbar);
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-
     }
 
     private void setNavigation() {
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransition = fragmentManager.beginTransaction();
                 switch (item.getItemId()) {
                     case R.id.tab_navigation_menu_home:
-                        Log.d(TAG, "tab_navigation_menu_home");
+                        fragmentTransition.replace(R.id.main_content, new FeedFragment()).commit();
                         break;
                     case R.id.tab_navigation_menu_search:
-                        Log.d(TAG, "tab_navigation_menu_search");
+                        fragmentTransition.replace(R.id.main_content, new SearchFragment()).commit();
                         break;
                     case R.id.tab_navigation_menu_add_post:
-                        Log.d(TAG, "tab_navigation_menu_add_post");
+                        fragmentTransition.replace(R.id.main_content, new PostFragment()).commit();
                         break;
                     case R.id.tab_navigation_menu_profile:
-                        Log.d(TAG, "tab_navigation_menu_profile");
+                        fragmentTransition.replace(R.id.main_content, new ProfileFragment()).commit();
                         break;
                 }
                 return true;
             }
         });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransition = fragmentManager.beginTransaction();
+        fragmentTransition.replace(R.id.main_content, new FeedFragment()).commit();
     }
 
     @Override
